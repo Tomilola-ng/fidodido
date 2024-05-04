c54506ca9bfdbb468ebd688d98f3990d566c9aae
Tomilola-ng <tee.o2809@gmail.com>
2024-05-04 23:28:03 +0100

    IMAGE

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,9 @@ export const metadata: Metadata = {
   description:
     "FIDO is a leading provider of innovative solutions tailored to meet the unique needs of startups and small businesses",
   openGraph: {
-    images: ["/public/fido-icon.png"],
+    images: [
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7cEuAwQ_5DwxSwDz2P4TnNT4M5Bv2zwSf6UPZW8ScVDjaNYUTorYoxMf5UfDCD7Wlb4ASeuPJ9A8Uug1-4k2lPO9R-rC89c_Yv4HP6f1Ac2rPGGw5INjIsyMLRKhqM1JUX8jgr17UosqTIfjRNLaBo6V2YobNCjJsZ_QGr2qTqQtp2ALuZf36VfS5Jw/s320/character.webp",
+    ],
     title: "Fido - The Business Associate you need",
     description:
       "FIDO is a leading provider of innovative solutions tailored to meet the unique needs of startups and small businesses",
