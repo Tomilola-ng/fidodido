b7f5f8f61b3eb951019fe2560f117818bd96e0d0
Tomilola-ng <tee.o2809@gmail.com>
2024-07-11 16:44:18 +0100

    UI_STUFF

--- app/join-our-team/page.tsx
+++ app/join-our-team/page.tsx
@@ -4,8 +4,12 @@ import myImg from "/public/flyer.webp";
 
 export default function JoinOurTeamPage() {
   return (
-    <section className="maxContainer flexCenter">
-      <Image src={myImg} alt="we are hiring flyer post" />
+    <section className="maxContainer flexCenter flex-col gap-y-4 pSm my-4">
+      <Image
+        src={myImg}
+        alt="we are hiring flyer post"
+        className="max-w-[640px] rounded-md"
+      />
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSdg6T7vQLVL2KToDyqv-D68Vh141dMzV8e8-gtYt3t8XLZ97g/viewform?embedded=true"
         width="640"
